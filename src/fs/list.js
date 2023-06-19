af93420683b06608db5b04810815cd95472b3917
Bek Shoyatbekov <92543629+ShBekzodbek@users.noreply.github.com>
2023-06-19 06:46:35 +0000

    list has been finished

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,14 @@
+import { readdir } from "fs/promises";
+
+import path from "path";
+
 const list = async () => {
-    // Write your code here 
+    try {
+      const list = await readdir(path.join("src", "fs", "files"));
+      console.log(list);
+    } catch (err) {
+        throw new Error("FS operation failed " + err);
+    }
 };
 
 await list();
