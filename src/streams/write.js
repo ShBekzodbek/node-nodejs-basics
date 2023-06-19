0e69253e5b9421c2a13f013ff0db3b4acc69295f
Bek Shoyatbekov <92543629+ShBekzodbek@users.noreply.github.com>
2023-06-19 09:55:15 +0000

    writing with stream has been finished

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,31 @@
+import { stdin, stdout } from "node:process";
+
+import { createInterface } from "node:readline/promises";
+
+import path from "node:path";
+
+import { createWriteStream } from "node:fs";
+
+import { once } from "node:events";
+
+const rl = createInterface({
+  input: stdin,
+  output: stdout,
+  
+});
+
 const write = async () => {
-    // Write your code here 
+  const writableStream = createWriteStream(
+    path.join("src", "streams", "files", "fileToWrite.txt")
+  );
+  try {
+    await once(writableStream, "open");
+    const input = await rl.question("Enter any input and ctrl + c to exit \n");
+    writableStream.write(input);
+    writableStream.close();
+  } catch (err) {
+    throw new Error("Writing with Steam Operation failed! " + err);
+  }
 };
 
 await write();
