09323274fef35f85b9994db48d0c267bdbfea3a9
Bek Shoyatbekov <92543629+ShBekzodbek@users.noreply.github.com>
2023-06-19 09:11:33 +0000

    reading with stream has been finished

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,24 @@
+import { stdout } from "node:process";
+
+import { createReadStream } from "fs";
+
+import path from "node:path";
+
+
 const read = async () => {
-    // Write your code here 
+     try {
+      let readable = createReadStream(
+        path.join("src", "streams", "files", "fileToRead.txt")
+      ).setEncoding("utf8");
+     let data = '';
+     for await (const chunk of readable) {
+       data += chunk;
+     }
+     stdout.write(data);
+     } catch (err) {
+      throw new Error("Stream Operation failed " + err);
+     }
+
 };
 
 await read();
