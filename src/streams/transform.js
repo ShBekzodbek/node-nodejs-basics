521e3178954cb2c9d1061b68caf1c6e2cd21189e
Bek Shoyatbekov <92543629+ShBekzodbek@users.noreply.github.com>
2023-06-19 10:07:46 +0000

    finished

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,25 @@
+import { Transform } from "node:stream";
+
+import { pipeline } from "stream/promises";
+
 const transform = async () => {
-    // Write your code here 
+try{ 
+     const reverseText = new Transform({
+       async transform(chunk, encoding, callback) {
+         const text = chunk.toString();
+         const reversed = text.split("").reverse().join("");
+         this.push(reversed);
+         callback();
+       },
+     });
+  await pipeline(
+    process.stdin,
+    reverseText,
+    process.stdout
+  );
+}catch(err){
+    throw new Error("Operation failed! " + err);
+}
 };
 
 await transform();
