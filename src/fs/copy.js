53232df333c096f1cb7b93595d796f5f25083e44
Bek Shoyatbekov <92543629+ShBekzodbek@users.noreply.github.com>
2023-06-19 06:24:05 +0000

    coping has been finished

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,11 @@
+import { cp } from "fs/promises";
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await cp("./src/fs/files", "./src/fs/files_copy", { recursive: true });
+  } catch (err) {
+    throw new Error("FS operation failed " + err);
+  }
 };
 
 await copy();
