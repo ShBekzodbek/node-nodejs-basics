6adea51e7deebcff44b4f77d87618b405b0841d6
Bek Shoyatbekov <92543629+ShBekzodbek@users.noreply.github.com>
2023-06-19 06:50:56 +0000

    readed

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import { readFile } from "fs/promises";
+
+import path from "path";
+
 const read = async () => {
-    // Write your code here 
+    try {
+       const data = await readFile(
+         path.join("src", "fs", "files", "fileToRead.txt"),
+         { encoding: "utf-8" }
+       );
+       console.log(data);
+    } catch (err) {
+        throw new Error("FS operation failed " + err);
+    }
 };
 
 await read();
