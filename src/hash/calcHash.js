4ec13243956b19391d097b3f13b208fce2be9252
Bek Shoyatbekov <92543629+ShBekzodbek@users.noreply.github.com>
2023-06-19 08:02:22 +0000

    hash function is ready to use

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { createHmac } from 'node:crypto';
+
+import { readFile } from "node:fs/promises";
+
+import path from "path";
+
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+      const text = await readFile(
+        path.join("src", "hash", "files", "fileToCalculateHashFor.txt"),
+        { encoding: "utf-8" }
+      );
+      const secret = "abcdefg";
+      const hash = createHmac("sha256", secret).update(text).digest("hex");
+      console.log(hash);
+    } catch (err) {
+        throw new Error("FS operation failed " + err);
+    }
 };
 
 await calculateHash();
