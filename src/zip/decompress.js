d38d11d2c21f53b97b3fca1cfb7a7d1ae75ed0a6
Bek Shoyatbekov <92543629+ShBekzodbek@users.noreply.github.com>
2023-06-19 10:21:32 +0000

    ready to use

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,28 @@
+import { createGunzip } from "node:zlib";
+
+import fs from "fs";
+
+import { pipeline } from "stream/promises";
+
+import path from "node:path";
+
 const decompress = async () => {
-    // Write your code here 
+  try {
+  let gunzip  = createGunzip();
+  let r = fs.createReadStream(
+    path.join("src", "zip", "files", "archive.gz")
+  );
+  let w = fs.createWriteStream(
+    path.join("src", "zip", "files", "fileToCompress.txt")
+  );
+  await pipeline(r, gunzip, w);
+  } catch (err) {
+    throw new Error("Operation failed" + err);
+  }
 };
+
+
+
+
 
 await decompress();
