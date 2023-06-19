d83b0c76da769cb99b3f2f6ed16a168c484d4eb4
Bek Shoyatbekov <92543629+ShBekzodbek@users.noreply.github.com>
2023-06-19 06:33:20 +0000

    rename function has been finished

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,16 @@
+import { rename as Rename } from "fs/promises";
+
+import path from "path";
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    await Rename(
+      path.join("src/fs", "files", "wrongFilename.txt"),
+      path.join("src/fs", "files", "properFilename.md")
+    );
+  } catch (err) {
+    throw new Error("FS operation failed " + err);
+  }
 };
 
 await rename();
